0ebc48e82f5932782df7dc941fcfe6dbf7a28e27
teruyayuu <zhaowuyouzuo@gmail.com>
2023-05-06 16:09:57 +0900

    Rails(課題)

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,10 +11,10 @@ class User
   def introduce
     <<~EOS
     
-     私の名前は#{@first_name + @last_name}です。
-     誕生日は#{@birthday}で、年齢は#{@age}歳。
-     出身地は#{@birthplace}で、趣味は#{@hobby}です。
+    私の名前は#{@first_name + @last_name}です。
+    誕生日は#{@birthday}で、年齢は#{@age}歳。
+    出身地は#{@birthplace}で、趣味は#{@hobby}です。
  
-      EOS
-    end
+    EOS
   end
+ end
