8f0ce1b40055cc4969943c84225c9631285c99d3
teruyayuu <zhaowuyouzuo@gmail.com>
2023-05-06 15:43:04 +0900

    Rails(課題)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,20 @@
-calss user
+class User
   def initialize
     @first_name = "Yuusaku"
     @last_name = "Teruya"
     @birthday = "1995/2/18"
-    @age = "28"
+    @age = 28
     @birthplace = "Okinawa/Naha"
-    @hobby = "reading"
+    @hobby = "読書"
   end
 
   def introduce
-   
-    私の名前は#{@first_name + @last_name}です。
-    誕生日は#{@birthday}で、年齢は#{@age}です。
-    出身地は#{@birthplace}で、趣味は#{@hobby}です。
-
+    <<~EOS
     
-  end
+     私の名前は#{@first_name + @last_name}です。
+     誕生日は#{@birthday}で、年齢は#{@age}歳。
+     出身地は#{@birthplace}で、趣味は#{@hobby}です。
+ 
+      EOS
+    end
   end
